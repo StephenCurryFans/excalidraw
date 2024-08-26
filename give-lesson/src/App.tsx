3f56e9afc961a316e5a91f5209ae95614ac709c7
stephen <sxccit@outlook.com>
2024-08-26 16:13:46 +0800

    feat: 画笔及上传图片功能实现

--- give-lesson/src/App.tsx
+++ give-lesson/src/App.tsx
@@ -1,7 +1,7 @@
 import { Excalidraw } from "../../packages/excalidraw";
 function App() {
   return (
-
+    // 实现一个画笔工具及上传图片功能
     <div style={{ height: "100vh" }}>
       <Excalidraw
         langCode="zh-CN"
